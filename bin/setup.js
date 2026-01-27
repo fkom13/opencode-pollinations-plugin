00e27d87a42a639b1692239597a4558d78593ad9
fkom13 <fkomprod@gmail.com>
2026-01-27 13:40:18 +0100

    Release v5.1.6: Fix setup script (ESM) & Update Docs

--- bin/setup.js
+++ bin/setup.js
@@ -1,7 +1,11 @@
 #!/usr/bin/env node
-const fs = require('fs');
-const path = require('path');
-const os = require('os');
+import fs from 'fs';
+import path from 'path';
+import os from 'os';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 console.log('🌸 Pollinations Plugin Setup');
 
